70f31caf471b820dbe784b3be9ff3e153ac9834c
Muhammad Qasim <qasim.tanveer81755@gmail.com>
2026-04-12 01:19:25 +0500

    Backend updated

--- Backend/app.js
+++ Backend/app.js
@@ -7,8 +7,14 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+  // Yahan apna Vercel wala final link dalna
+  origin: 'https://full-stack-product-management-system-qasim.vercel.app', 
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true,
+};
 
-app.use(cors())
+app.use(cors(corsOptions));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
